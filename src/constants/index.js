14c37b69bd25640789246f4ebdb30e4fe3801e90
Allan Musyoka <musyokaallan96@gmail.com>
2024-02-27 10:40:53 +0300

    Added constants folder

---
+++ src/constants/index.js
@@ -0,0 +1,86 @@
+import { desktopCuriosity, desktopDeepEarth, desktopFisheye, desktopFromAbove, desktopGrid, desktopNightArcade, desktopPocketBorealis, desktopSoccerTeam, iconFacebook, iconInstagram, iconPinterest, iconTwitter, mobileCuriosity, mobileDeepEarth, mobileFisheye, mobileFromAbove, mobileGrid, mobileNightArcade, mobilePocketBorealis, mobileSoccerTeam } from "../images"
+
+export const links = [
+    {
+        id: "link-1",
+        link: "About"
+    },
+    {
+        id: "link-2",
+        link: "Careers"
+    },
+    {
+        id: "link-3",
+        link: "Events"
+    },
+    {
+        id: "link-4",
+        link: "Products"
+    },
+    {
+        id: "link-5",
+        link: "Support"
+    },
+]
+
+export const creations = [
+    {
+        id: "creations-1",
+        desktopIcon: desktopDeepEarth,
+        mobileIcon: mobileDeepEarth
+    },
+    {
+        id: "creations-2",
+        desktopIcon: desktopNightArcade,
+        mobileIcon: mobileNightArcade
+    },
+    {
+        id: "creations-3",
+        desktopIcon: desktopSoccerTeam,
+        mobileIcon: mobileSoccerTeam
+    },
+    {
+        id: "creations-4",
+        desktopIcon: desktopGrid,
+        mobileIcon: mobileGrid
+    },
+    {
+        id: "creations-5",
+        desktopIcon: desktopFromAbove,
+        mobileIcon: mobileFromAbove
+    },
+    {
+        id: "creations-6",
+        desktopIcon: desktopPocketBorealis,
+        mobileIcon: mobilePocketBorealis
+    },
+    {
+        id: "creations-7",
+        desktopIcon: desktopCuriosity,
+        mobileIcon: mobileCuriosity
+    },
+    {
+        id: "creations-8",
+        desktopIcon: desktopFisheye,
+        mobileIcon: mobileFisheye
+    },
+]
+
+export const socials = [
+    {
+        id: "socials-1",
+        icon: iconFacebook,
+    },
+    {
+        id: "socials-2",
+        icon: iconTwitter,
+    },
+    {
+        id: "socials-3",
+        icon: iconPinterest,
+    },
+    {
+        id: "socials-4",
+        icon: iconInstagram,
+    },
+]
---
+++ src/images/index.js
@@ -0,0 +1,63 @@
+import iconClose from "./icon-close.svg";
+import iconFacebook from "./icon-facebook.svg";
+import iconHamburger from "./icon-hamburger.svg";
+import iconInstagram from "./icon-instagram.svg";
+import iconPinterest from "./icon-pinterest.svg";
+import iconTwitter from "./icon-twitter.svg";
+import logo from "./logo.svg";
+
+// Desktop Images
+
+import desktopCuriosity from "./desktop/image-curiosity.jpg";
+import desktopDeepEarth from "./desktop/image-deep-earth.jpg";
+import desktopFisheye from "./desktop/image-fisheye.jpg";
+import desktopFromAbove from "./desktop/image-from-above.jpg";
+import desktopGrid from "./desktop/image-grid.jpg";
+import desktopHero from "./desktop/image-hero.jpg";
+import desktopInteractive from "./desktop/image-interactive.jpg";
+import desktopNightArcade from "./desktop/image-night-arcade.jpg";
+import desktopPocketBorealis from "./desktop/image-pocket-borealis.jpg";
+import desktopSoccerTeam from "./desktop/image-soccer-team.jpg";
+
+// Mobile Images
+
+import mobileCuriosity from "./mobile/image-curiosity.jpg";
+import mobileDeepEarth from "./mobile/image-deep-earth.jpg";
+import mobileFisheye from "./mobile/image-fisheye.jpg";
+import mobileFromAbove from "./mobile/image-from-above.jpg";
+import mobileGrid from "./mobile/image-grid.jpg";
+import mobileHero from "./mobile/image-hero.jpg";
+import mobileInteractive from "./mobile/image-interactive.jpg";
+import mobileNightArcade from "./mobile/image-night-arcade.jpg";
+import mobilePocketBorealis from "./mobile/image-pocket-borealis.jpg";
+import mobileSoccerTeam from "./mobile/image-soccer-team.jpg";
+
+export {
+    iconClose,
+    iconFacebook,
+    iconHamburger,
+    iconInstagram,
+    iconPinterest,
+    iconTwitter,
+    logo,
+    desktopCuriosity,
+    desktopDeepEarth,
+    desktopFisheye,
+    desktopFromAbove,
+    desktopGrid,
+    desktopHero,
+    desktopInteractive,
+    desktopNightArcade,
+    desktopPocketBorealis,
+    desktopSoccerTeam,
+    mobileCuriosity,
+    mobileDeepEarth,
+    mobileFisheye,
+    mobileFromAbove,
+    mobileGrid,
+    mobileHero,
+    mobileInteractive,
+    mobileNightArcade,
+    mobilePocketBorealis,
+    mobileSoccerTeam
+}
