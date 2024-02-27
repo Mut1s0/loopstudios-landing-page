2a1cc74f92f3e8019875406a3b90f2a262294b0d
Allan Musyoka <musyokaallan96@gmail.com>
2024-02-27 09:18:02 +0300

    Added tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary Colors
+        white: "hsl( 0, 0%, 100% )",
+        black: "hsl( 0, 0%, 0% )",
+        darkGray: "hsl( 0, 0%, 55% )",
+        veryDarkGray: "hsl( 0, 0%, 41% )"
+      },
+      fontFamily: {
+        josefinSans: [ "Josefin Sans", "sans-serif" ],
+        alata: [ "Alata", "sans-serif" ]
+      },
+      fontWeight: {
+        josefinSansLight: "300",
+        alataRegular: "400",
+      },
+    },
+    screens: {
+      xs: "450px",
+      ss: "620px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1200px",
+      xl: "1700px",
+    }
   },
   plugins: [],
 }
